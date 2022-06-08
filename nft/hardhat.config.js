09fa4e3a88fed076b65f7e7581bf2cef44a4932d
Mahendra <mahendrabrowny@gmail.com>
2022-06-08 17:36:16 +0530

    first commit

---
+++ nft/hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.4",
+
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_API_KEY_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
