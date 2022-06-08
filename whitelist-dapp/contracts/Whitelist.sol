8e7b45e1b930efb4ec256734462dbab70990e675
Mahendra <mahendrabrowny@gmail.com>
2022-06-08 17:49:23 +0530

    logic for whitelist has been created

--- whitelist-dapp/contracts/Whitelist.sol
+++ whitelist-dapp/contracts/Whitelist.sol
@@ -1,3 +1,31 @@
 //SPDX-License-Identifier: Unlicense
 
 pragma solidity ^0.8.0;
+
+contract Whitelist {
+    uint256 public maxWhitelistedAddresses;
+
+    mapping(address => bool) public whitelistedAddresses;
+
+    uint256 public numAddressesWhitelisted;
+
+    constructor(uint8 _maxWhitelistedAddresses) {
+        maxWhitelistedAddresses = _maxWhitelistedAddresses;
+    }
+
+    function addAddressToWhitelist() public {
+        require(
+            !whitelistedAddresses[msg.sender],
+            "Sender has already been whitelisted"
+        );
+
+        require(
+            numAddressesWhitelisted < maxWhitelistedAddresses,
+            "More addresses can't be added, limit reached"
+        );
+
+        whitelistedAddresses[msg.sender] = true;
+
+        numAddressesWhitelisted += 1;
+    }
+}
