dedc456d5829f363fd7491fdd2c92698bc72fd8a
Mahendra <mahendrabrowny@gmail.com>
2022-06-09 21:32:17 +0530

    documented code is a better option

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -6,8 +6,9 @@ import { useEffect, useRef, useState } from "react";
 import { WHITELIST_CONTRACT_ADDRESS, abi } from "../constants";
 
 export default function Home() {
+  // walletConnected keep track of whether the user's wallet is connected or not
   const [walletConnected, setWalletConnected] = useState(false);
-
+  // joinedWhitelist keeps track of whether the current metamask address has joined the Whitelist or not
   const [joinedWhitelist, setJoinedWhitelist] = useState(false);
   // loading is set to true when we are waiting for a transaction to get mined
   const [loading, setLoading] = useState(false);
