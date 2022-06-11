fc38bd776a715c36134f4e6585a3fd5ed2e785fe
Mahendra <mahendrabrowny@gmail.com>
2022-06-11 13:02:22 +0530

    about to complete

---
+++ ico-dapp/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x0490a6E191Bac7D22dAC1616B871f70feF723DE9";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
